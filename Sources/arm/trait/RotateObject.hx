8c360dd01523f15651d61cfc20d7a996efeaf6ad
tong <tong@disktree.net>
2020-09-22 11:18:52 +0200

    Sync

---
+++ Sources/arm/trait/RotateObject.hx
@@ -0,0 +1,21 @@
+package arm.trait;
+
+class RotateObject extends Trait {
+	
+	@prop
+	public var axis = new Vec4(0,0,0,1);
+
+	@prop
+	public var speed = 0.01;
+	
+	public function new() {
+		super();
+		notifyOnInit( () -> {
+			notifyOnUpdate( update );
+		});
+	}
+	
+	function update() {
+		object.transform.rotate( axis, speed );
+	}
+}
