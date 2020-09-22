71330f3f58f72549c5993f189cd442f3c4d96f8a
tong <tong@disktree.net>
2020-09-22 14:20:39 +0200

    Release build opts

--- khaconfig.js
+++ khaconfig.js
@@ -1,10 +1,18 @@
-console.info("--- LD47 ---");
+
+const RELEASE = false;
+
+console.info(project.name+" "+project.version);
 
 project.addLibrary("tron");
+
+if (RELEASE) {
+    project.addParameter("-dce full");
+    project.addParameter("--no-traces");
+    project.addDefine("analyzer-optimize");
+}
 
 if (platform === Platform.HTML5) {
     project.addAssets("Assets/html5/**");
     project.targetOptions.html5.disableContextMenu = true;
     project.targetOptions.html5.canvasId = "viewport";
-    //project.targetOptions.html5.scriptName = appName;
 }
