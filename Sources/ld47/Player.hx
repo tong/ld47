165222b2fdad994104e5b9dbf85690e7e126c5fc
tong <tong@disktree.net>
2020-10-03 22:45:16 +0200

    Const player colors

--- Sources/ld47/Player.hx
+++ Sources/ld47/Player.hx
@@ -1,16 +1,18 @@
 package ld47;
 
 class Player {
+	public static final COLORS = [0xF50057, 0x00B0FF, 0xFFEA00, 0xFF3D00];
+
 	// public dynamic function onNavigate( direction : Float ) {}
 	public var index(default, null):Int;
 	public var name(default, null):String;
 	public var color(default, null):Int;
 	public var atom(default, null):Atom;
 
-	public function new(index:Int, name:String, color:Int) {
+	public function new(index:Int, name:String) {
 		this.index = index;
 		this.name = name;
-		this.color = color;
+		this.color = COLORS[index];
 	}
 
 	public function update() {
@@ -19,24 +21,35 @@ class Player {
 		if (gp.started('circle')) {
 			// TODO select electron
 		} else {
-
 			var v = new Vec2();
-			if (gp.started("left")) v.x = -1;
-			else if (gp.started("right")) v.x = 1;
-			if (gp.started("up")) v.y = 1;
-			else if (gp.started("down")) v.y = -1;
+			if (gp.started("left"))
+				v.x = -1;
+			else if (gp.started("right"))
+				v.x = 1;
+			if (gp.started("up"))
+				v.y = 1;
+			else if (gp.started("down"))
+				v.y = -1;
 
 			switch index {
-			case 0:
-				if( keyboard.started('left') ) v.x = -1;
-				else if( keyboard.started('right') ) v.x = 1;
-				if( keyboard.started('up') ) v.y = 1;
-				else if( keyboard.started('down') ) v.y = -1;
-			case 1:
-				if( keyboard.started('a') ) v.x = -1;
-				else if( keyboard.started('d') ) v.x = 1;
-				if( keyboard.started('w') ) v.y = 1;
-				else if( keyboard.started('s') ) v.y = -1;
+				case 0:
+					if (keyboard.started('left'))
+						v.x = -1;
+					else if (keyboard.started('right'))
+						v.x = 1;
+					if (keyboard.started('up'))
+						v.y = 1;
+					else if (keyboard.started('down'))
+						v.y = -1;
+				case 1:
+					if (keyboard.started('a'))
+						v.x = -1;
+					else if (keyboard.started('d'))
+						v.x = 1;
+					if (keyboard.started('w'))
+						v.y = 1;
+					else if (keyboard.started('s'))
+						v.y = -1;
 			}
 
 			navigateSelectionTowards(v);
@@ -75,28 +88,25 @@ class Player {
 			var atoms = Game.active.atoms.filter(a -> return a.player == this && a != atom);
 
 			if (atoms.length > 0) {
-                var bestAtom = null;
-                var bestScore = 0.0;
-                var bestDistance = 0.0;
+				var bestAtom = null;
+				var bestScore = 0.0;
+				var bestDistance = 0.0;
 
 				for (a in atoms) {
 					var distance = atom.object.transform.loc.distanceTo(a.object.transform.loc);
-					var direction =  new Vec2(a.object.transform.loc.x - atom.object.transform.loc.x, a.object.transform.loc.y - atom.object.transform.loc.y);
-                    var score = direction.dot(shootDirection) / distance;
-                    
+					var direction = new Vec2(a.object.transform.loc.x - atom.object.transform.loc.x, a.object.transform.loc.y - atom.object.transform.loc.y);
+					var score = direction.dot(shootDirection) / distance;
+
 					if (score > bestScore) {
 						bestScore = score;
-                        bestAtom = a;                        
-                        bestDistance = distance;
+						bestAtom = a;
+						bestDistance = distance;
 					}
-                }
+				}
 
-                if (bestAtom != null)
-                {
-                    selectAtom(bestAtom);
-                }   
-                
-				
+				if (bestAtom != null) {
+					selectAtom(bestAtom);
+				}
 			}
 		}
 	}
