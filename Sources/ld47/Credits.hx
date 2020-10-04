4564f9d7984a09f0dbb5efd2cd963a91c216abdd
tong <tong@disktree.net>
2020-10-04 19:37:23 +0200

    Add simple credits scene trait

---
+++ Sources/ld47/Credits.hx
@@ -0,0 +1,32 @@
+package ld47;
+
+import kha.graphics2.Graphics;
+
+class Credits extends Trait {
+
+	public function new() {
+		super();
+		notifyOnInit( () -> {
+            final sw = System.windowWidth();
+            final sh = System.windowHeight();
+            final keyboard = Input.getKeyboard();
+            var text = "Developed by disktree.net";
+            notifyOnUpdate( () -> {
+                if( keyboard.started('escape') ) {
+                    Scene.setActive( 'Mainmenu' );
+                }
+            });
+            notifyOnRender2D( g -> {
+                g.end();
+                g.color = 0xff000000;
+                g.fillRect( 0, 0, sw, sh );
+                g.font = UI.font;
+                g.fontSize = UI.fontSize;
+                g.color = 0xffffffff;
+                g.drawString( text, UI.fontSize, UI.fontSize ); 
+                g.begin( false );
+            });
+		});
+    }
+    
+}
