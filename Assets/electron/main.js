6ae6335aa19313d098cb07530ca6f0ed498e36bc
tong <tong@disktree.net>
2020-10-15 22:04:17 +0200

    Split htmml5 web/electron targets

--- Assets/electron/main.js
+++ Assets/electron/main.js
@@ -4,29 +4,28 @@ const { app, ipcMain, BrowserWindow } = require("electron");
 
 let win;
 
-//app.commandLine.appendSwitch('enable-unsafe-es3-apis');
 app.commandLine.appendSwitch('ignore-gpu-blacklist');
 
 app.on("ready",() => {
     win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    backgroundColor: '#000',
-    //resizable: true,
-    //show: false,
-    autoHideMenuBar: true,
-    backgroundThrottling: false,
-    //experimentalFeatures: true,
-    webPreferences: {
-      webgl: true,
-      nodeIntegration: true,
-      nodeIntegrationInWorker: true
-    }
-  });
-  win.loadFile(__dirname+'/index.html')
-  win.on("closed", () => {
-    win = null;
-  });
+      width: 1920,
+      height: 1080,
+      center: true,
+      //resizable: true,
+      icon: 'icon.svg',
+      autoHideMenuBar: true,
+      backgroundColor: '#000',
+      backgroundThrottling: false,
+      webPreferences: {
+        webgl: true,
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true
+      }
+    });
+    win.loadFile(__dirname+'/index.html')
+    win.on("closed", () => {
+      win = null;
+    });
 });
 
 app.on("window-all-closed", () => {
