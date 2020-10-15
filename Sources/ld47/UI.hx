de5c0cdab836adb2d8f3f2cb4ca0d9c889314031
tong <tong@disktree.net>
2020-10-15 08:12:30 +0200

    Transparent button bg

--- Sources/ld47/UI.hx
+++ Sources/ld47/UI.hx
@@ -50,9 +50,9 @@ class UI {
 		ACCENT_SELECT_COL: 0xff606060,
 		ARROW_SIZE: 5,
 		BUTTON_H: 40,
-		BUTTON_COL: 0xff000000,
-		BUTTON_HOVER_COL: 0xff000000,
-		BUTTON_PRESSED_COL: 0xff000000,
+		BUTTON_COL: 0x00ffffff,
+		BUTTON_HOVER_COL: 0x00ffffff,
+		BUTTON_PRESSED_COL: 0x00ffffff,
 		BUTTON_TEXT_COL: 0xffffffff,
 		CHECK_SELECT_SIZE: 40,
 		CHECK_SIZE: 48,
@@ -62,7 +62,7 @@ class UI {
 		ELEMENT_W: 100,
 		FILL_ACCENT_BG: false,
 		FILL_BUTTON_BG: false,
-		FILL_WINDOW_BG: true,
+		FILL_WINDOW_BG: false,
 		FONT_SIZE: 60,
 		HIGHLIGHT_COL: 0xff205d9c,
 		LABEL_COL: 0xffc8c8c8,
@@ -73,7 +73,7 @@ class UI {
 		TAB_W: 6,
 		TEXT_COL: 0xffe8e7e5,
 		TEXT_OFFSET: 8,
-		WINDOW_BG_COL: 0xff000000,
+		WINDOW_BG_COL: 0x00000000,
 		WINDOW_TINT_COL: 0xffffffff,
 	};
 
@@ -85,12 +85,12 @@ class UI {
 	public static var fontTitle : kha.Font;
 	public static var fontTitleSize = 60;
 
-	public static function init( done : Void->Void ) {
+	public static function init( ?done : Void->Void ) {
 		Data.getFont( 'mono.ttf', f -> {
 			font = f;
-			Data.getFont( "helvetica_neue_85.ttf", f -> {
+			Data.getFont( "title.ttf", f -> {
 				fontTitle = f;
-				done();
+				if( done != null ) done();
 			});
 		});
 	}
